a21364bfd640c244bddc956f0bb7c0eeaaf026e3
Aabhusan Aryal <aabhusanaryal@gmail.com>
2022-01-21 19:16:28 +0545

    Created some basic layouts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+  </div> -->
+  <div class="container-flex">
+    <LeftSidebar :playlists="playlists" />
+    <router-view />
+    <div style="min-width: var(--left-sidebar-width)" dummy-div></div>
   </div>
-  <router-view />
 </template>
 
+<script>
+import LeftSidebar from "@/components/LeftSidebar.vue";
+
+export default {
+  components: { LeftSidebar },
+  data() {
+    return {
+      playlists: [],
+    };
+  },
+  methods: {
+    createPlaylist(name, songs) {
+      let playlist = { name, songs };
+      this.playlists.push(playlist);
+    },
+  },
+  mounted() {
+    // Creating fake playlists
+    this.createPlaylist("Playlist-1", ["song1", "song2"]);
+    this.createPlaylist("Playlist-2", ["song1", "song2"]);
+    this.createPlaylist("Playlist-3", ["song1", "song2"]);
+    this.createPlaylist("Playlist-4", ["song1", "song2"]);
+    this.createPlaylist("Playlist-5", ["song1", "song2"]);
+    this.createPlaylist("Playlist-5", ["song1", "song2"]);
+  },
+};
+</script>
+
 <style>
+/* Importing fonts */
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+/* Setting variables */
+:root {
+  --left-sidebar-width: 350px;
+  --cyan: #35e6da;
+  --blue: #0496ff;
+}
+
+/* Removing margin and padding from all elements */
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container-flex {
+  display: flex;
+  flex-direction: row;
 }
 </style>
---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="left-sidebar">
+    <center>
+      <div id="logo"><img src="../assets/logo.png" alt="" /></div>
+    </center>
+    <div class="playlists-wrapper">
+      <div
+        class="playlist-card"
+        v-for="(playlist, index) in playlists"
+        :key="index"
+      >
+        <span class="text">
+          {{ playlist.name }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeftSidebar",
+  props: ["playlists"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#logo img {
+  width: 100px;
+  margin: 50px auto;
+}
+.left-sidebar {
+  left: 0;
+  min-width: var(--left-sidebar-width);
+  min-height: 100vh;
+  background: linear-gradient(
+    85.47deg,
+    rgba(53, 230, 218, 0.65) 10.3%,
+    rgba(5, 145, 245, 0.47) 95.71%
+  );
+  /* text-align: center; */
+}
+.playlists-wrapper {
+  /* Keeping distance from the logo */
+  margin-top: 40px;
+  display: flex;
+  /* Setting the flex-direction to column-reverse makes sure that the latest addition is at the top */
+  flex-direction: column-reverse;
+  flex-wrap: wrap;
+  align-items: center;
+}
+.playlist-card {
+  font-family: "Roboto", sans-serif;
+  font-size: 25px;
+  width: 80%;
+  background-color: #fff;
+  /* padding-left: 90px; */
+  margin: 10px 0;
+  border-radius: 40px;
+  height: 50px;
+  /* Centers the text inside the card vertically. The value should be same as height */
+  line-height: 50px;
+}
+
+.playlist-card:hover {
+  background-color: rgb(248, 248, 248);
+}
+
+.text {
+  padding-left: 35px;
+  /* font-weight: bold; */
+  color: var(--cyan);
+}
+</style>
