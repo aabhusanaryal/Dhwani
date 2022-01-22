754b6dbe842bfa6e7b662ab87bab7d3faa76dfab
Aabhusan Aryal <aabhusanaryal@gmail.com>
2022-01-22 19:52:36 +0545

    Updated container settings for Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,6 +23,52 @@
       iusto, tempore omnis obcaecati deserunt atque iure in suscipit temporibus,
       natus nam maxime voluptatum.
     </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsum
+      quod ea asperiores laboriosam dolores odit officia beatae voluptate atque
+      quaerat sequi in quam esse quis, quas architecto dolorum harum vel
+      eligendi impedit? Quam odit neque architecto eveniet quidem ullam quae
+      accusamus voluptate! Aut reprehenderit dolor dolorum animi, sit optio esse
+      nam? Odit rerum nemo illo fuga? Delectus a velit suscipit quibusdam fuga
+      iusto modi veritatis voluptate impedit corporis necessitatibus eos
+      corrupti cumque accusantium nihil sit praesentium eveniet magni, inventore
+      esse, pariatur accusamus. Eum ab quibusdam sed dolore soluta tempore optio
+      maiores neque mollitia cupiditate, illo, cum, ipsa rem ullam aspernatur
+      non dignissimos ipsam! Reprehenderit corrupti in itaque velit libero
+      exercitationem porro dolor totam a labore nesciunt quam quidem aperiam
+      tempore incidunt veniam sit ipsum accusantium molestiae, nobis repellat
+      quis, ab aspernatur. Recusandae voluptatem dolores sapiente ipsam quam
+      unde, debitis nemo impedit fuga cupiditate fugit commodi totam non
+      incidunt alias exercitationem? Modi a iste necessitatibus ullam quod
+      beatae cupiditate recusandae nam dolorum rerum temporibus quos, omnis
+      nesciunt nihil sapiente explicabo eaque veniam. Natus deleniti, quas dicta
+      nobis, ducimus, a sit accusamus eveniet rerum debitis similique. Iste,
+      iusto, tempore omnis obcaecati deserunt atque iure in suscipit temporibus,
+      natus nam maxime voluptatum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsum
+      quod ea asperiores laboriosam dolores odit officia beatae voluptate atque
+      quaerat sequi in quam esse quis, quas architecto dolorum harum vel
+      eligendi impedit? Quam odit neque architecto eveniet quidem ullam quae
+      accusamus voluptate! Aut reprehenderit dolor dolorum animi, sit optio esse
+      nam? Odit rerum nemo illo fuga? Delectus a velit suscipit quibusdam fuga
+      iusto modi veritatis voluptate impedit corporis necessitatibus eos
+      corrupti cumque accusantium nihil sit praesentium eveniet magni, inventore
+      esse, pariatur accusamus. Eum ab quibusdam sed dolore soluta tempore optio
+      maiores neque mollitia cupiditate, illo, cum, ipsa rem ullam aspernatur
+      non dignissimos ipsam! Reprehenderit corrupti in itaque velit libero
+      exercitationem porro dolor totam a labore nesciunt quam quidem aperiam
+      tempore incidunt veniam sit ipsum accusantium molestiae, nobis repellat
+      quis, ab aspernatur. Recusandae voluptatem dolores sapiente ipsam quam
+      unde, debitis nemo impedit fuga cupiditate fugit commodi totam non
+      incidunt alias exercitationem? Modi a iste necessitatibus ullam quod
+      beatae cupiditate recusandae nam dolorum rerum temporibus quos, omnis
+      nesciunt nihil sapiente explicabo eaque veniam. Natus deleniti, quas dicta
+      nobis, ducimus, a sit accusamus eveniet rerum debitis similique. Iste,
+      iusto, tempore omnis obcaecati deserunt atque iure in suscipit temporibus,
+      natus nam maxime voluptatum.
+    </p>
   </div>
 </template>
 
@@ -37,7 +83,6 @@ export default {
 
 <style scoped>
 .center-container {
-  margin: 100px;
   font-size: 25px;
   overflow-y: scroll;
   overflow-x: hidden;
