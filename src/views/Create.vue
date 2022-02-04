305365120c19907d2ea79b62e2eb7b04438fa142
Mohit Agarwal <mohit1999@live.in>
2022-02-04 20:01:07 +0545

    Css added to create

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,40 +1,42 @@
 <template>
   <form @submit.prevent="addPlaylist">
-    <input
-      type="text"
-      placeholder="Enter Playlist Name"
-      v-model="playlistName"
-      class="name-input"
-    />
-    <button class="add" type="submit">+</button>
-    <br />
-    <span id="errMsg">
-      {{ errMsg }}
-    </span>
-    <table>
-      <thead>
-        <tr>
-          <th class="SN TH">#</th>
-          <th class="TH">Name</th>
-          <th class="TH">Artist</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(song, index) in playlists[0].songs" :key="index">
-          <td class="SN">{{ index + 1 }}</td>
-          <td>{{ song.name }}</td>
-          <td>{{ song.artist }}</td>
-          <td class="checkbox-wrapper">
-            <input
-              type="checkbox"
-              :value="song"
-              v-model="addedSongs"
-              name="songCheckbox"
-            />
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <div class="main-wrapper">
+        <input
+        type="text"
+        placeholder="Enter Playlist Name"
+        v-model="playlistName"
+        class="name-input"
+        />
+        <button class="add" type="submit">+</button>
+        <br />
+        <span id="error-msg">
+        {{ errMsg }}
+        </span>
+        <table>
+            <thead>
+                <tr>
+                    <th class="sn">#</th>
+                    <th>Name</th>
+                    <th>Artist</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(song, index) in playlists[0].songs" :key="index">
+                <td class="sn">{{ index + 1 }}</td>
+                    <td>{{ song.name }}</td>
+                    <td>{{ song.artist }}</td>
+                    <td class="checkbox-wrapper">
+                        <input
+                        type="checkbox"
+                        :value="song"
+                        v-model="addedSongs"
+                        name="songCheckbox"
+                        />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+      </div>
   </form>
 </template>
 
@@ -66,51 +68,9 @@ export default {
 
 <style scoped>
 form {
-  margin: 10px auto;
-}
-table {
-  font-family: "Roboto", sans-serif;
-  color: #4c4e52;
-  display: block;
-  border-collapse: separate;
-  border-spacing: 0 20px;
-}
-tbody {
-  display: block;
-  overflow-y: scroll;
-  max-height: 600px;
-}
-td,
-th {
-  width: 200px;
-  text-align: left;
-}
-.SN {
-  width: 100px;
-}
-th {
-  color: #6f7378;
-  font-weight: lighter;
-}
-.add {
-  color: white;
-  height: fit-content;
-  font-family: "Roboto", sans-serif;
-  font-size: 25px;
-  width: 50px;
-  /* background: var(--active-gradient); */
-  background-image: var(--merged-gradient);
-  background-size: 200%;
-  background-position: left;
-  border: none;
-  margin: 10px 25px;
-  border-radius: 30px;
-  height: 50px;
-  transition: 0.5s;
-}
-.add:hover {
-  background-position: right;
-  cursor: pointer;
+    margin: 10px auto;
+    font-size:23px ;
+    font-family: "Roboto", sans-serif;
 }
 .name-input {
   font-family: Roboto;
@@ -124,36 +84,78 @@ th {
   border-right: none;
   border-left: none;
   border-bottom-width: 5px;
-  border-image: linear-gradient(
-      93.64deg,
-      #35e6da 0.98%,
-      rgba(5, 145, 245, 0.42),
-      #35e6da 0.98%,
-      rgba(16, 55, 245, 0.5) 119.65%
-    )
-    100;
+  border-image: var(--primary-gradient) 100;
   border-image-width: 0 0 3px 0;
 }
 input:focus-visible {
-  border-top: none;
-  border-right: none;
-  border-left: none;
-  border-image: linear-gradient(
-      93.64deg,
-      #35e6da 0.98%,
-      rgba(5, 145, 245, 0.42),
-      #35e6da 0.98%,
-      rgba(16, 55, 245, 0.5) 119.65%
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    border-image: linear-gradient(
+        93.64deg,
+        #35e6da 0.98%,
+        rgba(5, 145, 245, 0.42),
+        #35e6da 0.98%,
+        rgba(16, 55, 245, 0.5) 119.65%
     )
     100;
-  border-image-width: 0 0 4px 0;
-  transition: 0.05s;
-  outline: none;
+    border-image-width: 0 0 4px 0;
+    transition: 0.05s;
+    outline: none;
 }
 input::placeholder {
-  color: rgba(178, 180, 189, 255);
+    color: rgba(178, 180, 189, 255);
 }
-#errMsg {
-  color: red;
+.add {
+    color: white;
+    height: fit-content;
+    font-size: 30px;
+    font-family: "Roboto", sans-serif;
+    width: 50px;
+    background-image: var(--merged-gradient);
+    background-size: 200%;
+    background-position: left;
+    border: none;
+    margin: 10px 25px;
+    border-radius: 30px;
+    height: 50px;
+    transition: 0.5s;
+}
+.add:hover {
+    background-position: right;
+    cursor: pointer;
+}
+#error-msg {
+    color: red;
+}
+table {
+  color: #646569;
+  display: block;
+  border-collapse: separate;
+  border-spacing: 0 20px;
+}
+tbody {
+  display: block;
+  overflow-y: scroll;
+  max-height: 600px;
+  /* width: fit-content */
+}
+thead{
+    display: block;
+    height: 50px;
+}
+td,th {
+  width: 250px;
+  text-align: left;
+}
+th {
+    color: #84898f;
+    font-weight: lighter;
+}
+.sn {
+  width: 100px;
+}
+.checkbox-wrapper{
+    width:50px;
 }
 </style>
