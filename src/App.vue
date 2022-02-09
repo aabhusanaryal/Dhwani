d6518bb1a7d5f3829e1ae1f41556f641d05179fc
Prabigya Acharya <acharyaprabigya@gmail.com>
2022-02-09 12:57:02 +0545

    Height of grid container fixed and current playlist name shown

--- src/App.vue
+++ src/App.vue
@@ -250,7 +250,7 @@ html {
   display: grid;
   height: 100vh;
   width: 100vw;
-  grid-template-rows: 0.25fr 2fr 0.8fr;
+  grid-template-rows: 10% 70% 20%;
   grid-template-columns: 20% 60% 20%;
   grid-template-areas:
     "leftcontainer topcontainer rightcontainer"
@@ -275,6 +275,7 @@ html {
   grid-area: bottomcontainer;
   display: flex;
   justify-content: center;
+  outline: 2px solid blue;
 }
 @media screen and (max-width: 768px) {
   .maincontainer {
