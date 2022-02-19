e98245a08a41126db1e79f133c36b15de5c129ea
Prabigya Acharya <acharyaprabigya@gmail.com>
2022-02-19 22:33:26 +0545

    Added more songs and changed the default volume to 30

--- src/App.vue
+++ src/App.vue
@@ -464,6 +464,116 @@ export default {
       isFav: null,
       duration: 227,
     };
+    let backyardmoon = {
+      name: "Backyard Moon",
+      path: "Backyard Moon - Props",
+      artist: "Props",
+      artistCover:"https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/9c/08/b3/9c08b349-2045-ec29-4a2c-e89e3ea72270/5059806887495_cover.jpg/400x400cc.jpg",
+      cover:
+        "https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/9c/08/b3/9c08b349-2045-ec29-4a2c-e89e3ea72270/5059806887495_cover.jpg/400x400cc.jpg",
+      genres: ["Indie"],
+      audio: null,
+      isFav: null,
+      duration: 157,
+    };
+    let onandon = {
+      name: "On & On",
+      path: "Cartoon - On & On",
+      artist: "Cartoon",
+      artistCover:"https://ncsmusic.s3.eu-west-1.amazonaws.com/artists/000/000/073/325x325/1597148402_SavquoNo8D_Cartoon.png",
+      cover:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/152/1000x0/on-on-feat-daniel-levi-1586947008-oeI24in2Ga.jpg",
+      genres: ["Dreamy", "EDM"],
+      audio: null,
+      isFav: null,
+      duration: 294,
+    };
+    let invincible = {
+      name: "Invincible",
+      path: "DEAF KEV - Invincible",
+      artist: "DEAV KEV",
+      artistCover: "https://ncsmusic.s3.eu-west-1.amazonaws.com/artists/000/000/103/325x325/1597148851_NLkqcNRLl3_DEAF-KEV.png",
+      cover:
+        "https://i1.sndcdn.com/artworks-000224592805-nhfd2x-t500x500.jpg",
+      genres: ["Dubstep", "Hopeful"],
+      audio: null,
+      isFav: null,
+      duration: 273,
+    };
+    let myheart = {
+      name: "My Heart",
+      path: "My Heart",
+      artist: "Different Heaven & EH!DE",
+      artistCover: "https://ncsmusic.s3.eu-west-1.amazonaws.com/artists/000/000/110/325x325/1597149100_6jB4ktu67O_Different-Heaven.png",
+      cover:
+        "https://i1.sndcdn.com/artworks-000372362241-tdudx0-t500x500.jpg",
+      genres: ["Energetic"],
+      audio: null,
+      isFav: null,
+      duration: 267,
+    };
+    let flowers = {
+      name: "Flowers",
+      path: "Flowers - Sakura Girl",
+      artist: "Sakura Girl",
+      artistCover: "https://i.scdn.co/image/ab67616d0000b273ed63305d487d528ab8495be9",
+      cover:
+        "https://i.scdn.co/image/ab67616d0000b273ed63305d487d528ab8495be9",
+      genres: ["Chill", "Happy"],
+      audio: null,
+      isFav: null,
+      duration: 225,
+    };
+    let heroestonight = {
+      name: "Heroes Tonight",
+      path: "Janji - Heroes Tonight",
+      artist: "Janji",
+      artistCover: "https://ncsmusic.s3.eu-west-1.amazonaws.com/artists/000/000/198/325x325/janji-1586942350-QUHR3pfFSM.jpg",
+      cover:
+        "https://i1.sndcdn.com/artworks-000119717450-w3zyh8-t500x500.jpg",
+      genres: ["House", "Hopeful", "Energetic"],
+      audio: null,
+      isFav: null,
+      duration: 208,
+    };
+    let sincethatsummerbegan = {
+      name: "Since That Summer Began",
+      path: "Since That Summer Began - Broke in Summer",
+      artist: "Broke in Summer",
+      artistCover:"https://brokeinsummer.com/wp-content/uploads/Since-that-summer-began-Cover-1080x1080.jpg",
+      cover:
+        "https://brokeinsummer.com/wp-content/uploads/Since-that-summer-began-Cover-1080x1080.jpg",
+      genres: ["Chill"],
+      audio: null,
+      isFav: null,
+      duration: 125,
+    };
+    let sunriseinyoureyes = {
+      name: "Sunrise In Your Eyes",
+      path: "Sunrise In Your Eyes - Scandinavianz",
+      artist: "Scandinavianz",
+      artistCover: "https://i1.sndcdn.com/artworks-pTLJ0ROg3lXumjdC-68qOqA-t500x500.jpg",
+      cover:
+        "https://i1.sndcdn.com/artworks-pTLJ0ROg3lXumjdC-68qOqA-t500x500.jpg",
+      genres: ["Chill", "Happy"],
+      audio: null,
+      isFav: null,
+      duration: 158,
+    };
+    let sunsetdreams = {
+      name: "Sunset Dreams",
+      path: "Sunset Dreams - Cheel",
+      artist: "Cheel",
+      artistCover: "https://i1.sndcdn.com/artworks-qWWKONMyNiDLBvxQ-Rpkyxw-t500x500.jpg", 
+      cover:
+        "https://i1.sndcdn.com/artworks-qWWKONMyNiDLBvxQ-Rpkyxw-t500x500.jpg",
+      genres: ["Indie"],
+      audio: null,
+      isFav: null,
+      duration: 161,
+    };
+  
+
 
     this.createPlaylist("All Songs", [
       savage,
@@ -478,6 +588,15 @@ export default {
       symphony,
       whatyoulike,
       newstyle,
+      backyardmoon,
+      onandon,
+      invincible,
+      myheart,
+      flowers,
+      heroestonight,
+      sincethatsummerbegan,
+      sunriseinyoureyes,
+      sunsetdreams,
     ]);
     this.createPlaylist("Favourites", []);
     this.createPlaylist("Sunshine & Rain", []);
